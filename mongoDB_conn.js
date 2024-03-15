0939246ca52698dd97e7bebbe4c0a06547ddaf13
mvs1903 <101926677+mvs1903@users.noreply.github.com>
2024-03-15 18:53:39 +0530

    db hosted

--- mongoDB_conn.js
+++ mongoDB_conn.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/UrlShortener')
+mongoose.connect('mongodb+srv://mvs81349:jG0CCGQ9knydKrVA@cluster0.m2ouoe3.mongodb.net/')
   .then(() => console.log('Connected!'));
 module.exports =mongoose
